3bdd9693bf5044e0ab078b36b64a45f070a376af
pankaj <pkp20022@gmail.com>
2024-08-04 10:35:07 +0530

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { notFound } from 'next/navigation'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import ReactMarkdown from 'react-markdown'
 
 interface Props {
     params: {
@@ -26,8 +27,8 @@ const IssueDetailPage = async ({params} : Props) => {
         <IssueStatusBadge status={issue.status}/>
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className=' prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   )
